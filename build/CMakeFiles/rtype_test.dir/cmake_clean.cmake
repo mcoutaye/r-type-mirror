a3dbea9681de24c4d5405d8ab34bb17fa2cb200e
mcoutaye <mathis.coutaye@epitech.eu>
2025-11-20 09:33:20 +0400

    TEST: test first CI/CD and ADD: add basic Cmake to test

---
+++ build/CMakeFiles/rtype_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtype_test.dir/link.d"
+  "CMakeFiles/rtype_test.dir/main.cpp.o"
+  "CMakeFiles/rtype_test.dir/main.cpp.o.d"
+  "rtype_test"
+  "rtype_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtype_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "CMake test OK !" << std::endl;
+    return 0;
+}

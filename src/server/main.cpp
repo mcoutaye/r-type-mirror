c8eb94d97cfb084007043766e5c2958301e410c2
DrakoGabi <gabriel.villemonte@epitech.eu>
2025-12-15 10:52:26 +0400

    [FIX] called to deleted function in main

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -5,16 +5,16 @@
 ** Server Main
 */
 
-#include "Args.hpp"
+
 #include "NetworkManager.hpp"
 #include <iostream>
 #include <SFML/System.hpp>
 
 int main(int argc, char *argv[])
 {
-    Args args(argc, argv);
-    auto config = args.parse();
-    Ntw::NetworkManager network(config.port);
+    // Args args(argc, argv);
+    // auto config = args.parse();
+    // Ntw::NetworkManager network(config.port);
     network.start();
     network.join();
     return 0;
